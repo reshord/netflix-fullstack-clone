4158b9a220bf64516b5148f68c8f31b52fe0374e
Dmitriy <littlebig11234@gmail.com>
2023-06-16 23:09:04 +0300

    add github and google auth methods

--- netflix-clone-frontend/pages/auth.tsx
+++ netflix-clone-frontend/pages/auth.tsx
@@ -4,6 +4,8 @@ import axios from 'axios'
 import {signIn} from 'next-auth/react'
 import { useRouter } from "next/router";
 import {message} from 'antd'
+import {FcGoogle} from 'react-icons/fc'
+import {FaGithub} from 'react-icons/fa'
 
 const AuthPage = () => {
 
@@ -18,31 +20,6 @@ const AuthPage = () => {
     const toggleVariant = useCallback(() => {
         setVariant((currentVariant) => currentVariant === 'login' ? 'register' : 'login')
     }, [])
-
-    // const onSubmit = (data: {username: string, email: string, password: string}) => {
-    //     console.log(data)
-    // }
-
-    const register = useCallback(async () => {
-        try {
-            await axios.post('/api/register', {
-                email,
-                name,
-                password
-            })
-            message.success({
-                duration: 2,
-                content: 'Register Successful'
-            })
-        }
-        catch(e) {
-            console.error(e)
-            message.error({
-                duration: 2,
-                content: 'Something went wrong'
-            })
-        }
-    }, [email, name, password])
 
     const login = useCallback(async () => {
         try {
@@ -66,6 +43,69 @@ const AuthPage = () => {
             })
         }
     }, [email, password])
+
+    const register = useCallback(async () => {
+        try {
+            await axios.post('/api/register', {
+                email,
+                name,
+                password
+            })
+            login()
+            message.success({
+                duration: 2,
+                content: 'Register Successful'
+            })
+        }
+        catch(e) {
+            console.error(e)
+            message.error({
+                duration: 2,
+                content: 'Something went wrong'
+            })
+        }
+    }, [email, name, password])
+
+    const signInWithMethods = async (method: string) => {
+        switch(method) {
+            case 'github': 
+                try {
+                    await signIn('github', {callbackUrl: '/'})
+                    message.success({
+                        duration: 2,
+                        content: 'Login with GitHub method successful'
+                    })
+                    router.push('/')
+                }
+                catch(e) {
+                    console.log(e)
+                    message.success({
+                        duration: 2,
+                        content: 'Something went wrong'
+                    })
+                }
+            case 'google':
+                try {
+                    await signIn('google', {callbackUrl: '/'})
+                    message.success({
+                        duration: 2,
+                        content: 'Login with Google method successful'
+                    })
+                    router.push('/')
+                }
+                catch(e) {
+                    console.log(e)
+                    message.success({
+                        duration: 2,
+                        content: 'Something went wrong'
+                    })
+                }
+                
+            default: 
+                break
+        }
+    }
+    
 
     return ( 
         <div className="relative h-full w-full bg-[url('/images/hero.jpg')] bg-no-repeat bg-center bg-fixed bg-cover">
@@ -105,6 +145,18 @@ const AuthPage = () => {
                         <button onClick={variant === 'login' ? login : register} className="bg-red-600 py-3 text-white rounded-md w-full mt-10 hover:bg-red-700 font-bold transition">
                             {variant === 'login' ? 'Login' : 'Register'}
                         </button>
+                        <div className="flex flex-row items-center gap-4 mt-8 justify-center">
+                                <div 
+                                    onClick={() => signInWithMethods('google')}
+                                    className="w-10 h-10 bg-white rounded-full flex items-center justify-center cursor-pointer hover:opacity-80 transition">
+                                        <FcGoogle size={25} />                                     
+                                </div>
+                                <div 
+                                    onClick={() => signInWithMethods('github')} 
+                                    className="w-10 h-10 bg-white rounded-full flex items-center justify-center cursor-pointer hover:opacity-80 transition">
+                                        <FaGithub size={25}/>
+                                </div>
+                        </div>
                         <p className="text-neutral-500 mt-12">
                                 {variant === 'login' ? 'First time using Netflix?' : 'Already have an account?'}
                                 <span onClick={toggleVariant} className="text-white ml-1 font-bold hover:underline cursor-pointer">
